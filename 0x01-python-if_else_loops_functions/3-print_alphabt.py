6fffad0d149ba46f67e9eb6ff18307b54e82bab1
Adesoji Alu <adesoji.alu@gmail.com>
2022-07-12 13:28:06 +0100

    Add files via upload

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""
-Print the ASCII Alphabet in lowercase with letter e and q
-"""
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    ascii_num = ord(c)
-    if ascii_num >= 97 and ascii_num <= 122:
+    """Print a string in lowercase."""
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
-    return False
+    else:
+        return False
